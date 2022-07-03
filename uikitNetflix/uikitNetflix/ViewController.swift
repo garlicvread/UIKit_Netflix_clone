534cdf9bd6e1be345dfbe49899694697684c1fcc
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-07-03 12:46:04 +0900

    initial commit

---
+++ uikitNetflix/uikitNetflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uikitNetflix
+//
+//  Created by 김제필 on 7/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

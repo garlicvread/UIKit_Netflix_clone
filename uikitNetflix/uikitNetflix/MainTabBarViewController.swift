3ddeadf5bd125ea023be9ed357cfa9109a3975ee
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-07-03 12:55:32 +0900

    Controller directory has been added.

---
+++ uikitNetflix/uikitNetflix/MainTabBarViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  uikitNetflix
+//
+//  Created by 김제필 on 7/3/22.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemYellow
+
+    }
+
+
+}
+

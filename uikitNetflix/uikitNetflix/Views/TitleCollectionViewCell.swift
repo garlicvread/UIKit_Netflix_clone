e782da046d2a08b4e971548d293d88b8d717c160
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-07-06 16:41:46 +0900

    TitleCollectionViewCell.swift has been added.

--- uikitNetflix/uikitNetflix/Views/TitleCollectionViewCell.swift
+++ uikitNetflix/uikitNetflix/Views/TitleCollectionViewCell.swift
@@ -6,7 +6,38 @@
 //
 
 import UIKit
+import SDWebImage
 
 class TitleCollectionViewCell: UICollectionViewCell {
-    
+    static let identifier = "TitleCollectionViewCell"
+
+    // imageView to show the posters
+    // closure(anonuymous function, lambda function) pattern
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill  // fill the entire cell regardless the size of the cell.
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+
+    // 3rd party library to retrieve/cache the poster images
+    // When dequeue a cell from the collectionView -> update the poster for each cell: public function
+
+    public func configure(with model: String) {
+        guard let url = URL(string: model) else {return}
+        posterImageView.sd_setImage(with: url, completed: nil)
+    }
 }

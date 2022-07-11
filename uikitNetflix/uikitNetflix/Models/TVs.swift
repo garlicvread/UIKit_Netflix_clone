d55952247d6254f2a05d4c768178cd13c834330f
Kim, Raymond <82295573+garlicvread@users.noreply.github.com>
2022-07-11 16:17:33 +0900

    [HOTFIX] Build failuer is fixed.

--- uikitNetflix/uikitNetflix/Models/TVs.swift
+++ uikitNetflix/uikitNetflix/Models/TVs.swift
@@ -1,5 +1,5 @@
 //
-//  Movies.swift
+//  TVs.swift
 //  uikitNetflix
 //
 //  Created by 김제필 on 7/5/22.
@@ -7,11 +7,11 @@
 
 import Foundation
 
-struct ResponsesAboutTheTrendingTitles: Codable {
-    let results: [Title]
+struct ResponsesAboutTheTrendingTVs: Codable {
+    let results: [TV]
 }
 
-struct Title: Codable {
+struct TV: Codable {
     let id: Int
     let media_type: String?
     let original_name: String?
@@ -22,3 +22,4 @@ struct Title: Codable {
     let vote_average: Double?
     let release_date: String?
 }
+
